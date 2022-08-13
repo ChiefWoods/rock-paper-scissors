29af18aa30b2b2beeb8e28f88f92fbe54d693805
Chii Yuen <chii.yuen@hotmail.com>
2022-08-14 02:25:38 +0800

    Complete main code

---
+++ script.js
@@ -0,0 +1,82 @@
+function getComputerChoice() {
+  let choice;
+  let number = Math.floor(Math.random() * 10);
+  if (number <= 3) {
+    choice = "rock";
+  }
+  else if (number > 3 && number <= 6) {
+    choice = "paper";
+  }
+  else choice = "scissors";
+  return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  console.log(`You chose ${playerSelection} and the computer chose ${computerSelection}...`);
+
+  switch (playerSelection) {
+    case 'rock':
+      if (playerSelection == computerSelection) {
+        console.log("No one wins this round");
+        break;
+      }
+      else if (computerSelection == "paper") {
+        console.log("You lose! Paper beats rock");
+        computerWins += 1;
+        break;
+      }
+      else console.log("You win! Rock beats scissors");
+      playerWins += 1;
+      break;
+    case 'paper':
+      if (playerSelection == computerSelection) {
+        console.log("No one wins this round");
+        break;
+      }
+      else if (computerSelection == "scissors") {
+        console.log("You lose! Scissors beats paper");
+        computerWins += 1;
+        break;
+      }
+      else console.log("You win! Paper beats rock");
+      playerWins += 1;
+      break;
+    case 'scissors':
+      if (playerSelection == computerSelection) {
+        console.log("No one wins this round");
+        break;
+      }
+      else if (computerSelection = "rock") {
+        console.log("You lose! Rock beats scissors");
+        computerWins += 1;
+        break;
+      }
+      else console.log("You win! Scissors beats paper");
+      playerWins += 1;
+      break;
+  }
+}
+
+function game() {
+  var playerSelection;
+  for (let i = 0; i < 5; i++) {
+    playerSelection = prompt("Enter 'rock', 'paper' or 'scissors':");
+    playerSelection = playerSelection.toLowerCase();
+    while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
+      playerSelection = prompt("Invalid input! Please enter either 'rock', 'paper' or 'scissors':");
+      playerSelection = playerSelection.toLowerCase();
+    }
+    playRound(playerSelection, getComputerChoice());
+  }
+  if (playerWins > computerWins) {
+    console.log("Congratualations! You won the best of 5 in Rock-Paper-Scissors!")
+  }
+  else if (playerWins < computerWins) {
+    console.log("Bummer! Better luck next time!")
+  }
+  else console.log("Tie! Both you and the computer ended the game with the same points")
+}
+
+var playerWins = 0;
+var computerWins = 0;
+game();
