2d4bd01d8c61eebaa6821e5f99aabe0751dbfa3b
Chii Yuen <chii.yuen@hotmail.com>
2022-08-14 18:47:46 +0800

    Reformat code and bug fix

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 function getComputerChoice() {
   let choice;
   let number = Math.floor(Math.random() * 10);
-  if (number <= 3) {
-    choice = "rock";
-  }
-  else if (number > 3 && number <= 6) {
-    choice = "paper";
-  }
-  else choice = "scissors";
-  return choice;
+  return (number <= 3 ? choice = "rock" : number > 3 && number <= 6 ? choice = "paper" : choice = "scissors");
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -46,7 +39,7 @@ function playRound(playerSelection, computerSelection) {
         console.log("No one wins this round");
         break;
       }
-      else if (computerSelection = "rock") {
+      else if (computerSelection == "rock") {
         console.log("You lose! Rock beats scissors");
         computerWins += 1;
         break;
@@ -60,21 +53,19 @@ function playRound(playerSelection, computerSelection) {
 function game() {
   var playerSelection;
   for (let i = 0; i < 5; i++) {
-    playerSelection = prompt("Enter 'rock', 'paper' or 'scissors':");
-    playerSelection = playerSelection.toLowerCase();
+    playerSelection = prompt("Enter 'rock', 'paper' or 'scissors':").toLowerCase();
     while (playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors') {
-      playerSelection = prompt("Invalid input! Please enter either 'rock', 'paper' or 'scissors':");
-      playerSelection = playerSelection.toLowerCase();
+      playerSelection = prompt("Invalid input! Please enter either 'rock', 'paper' or 'scissors':").toLowerCase();
     }
     playRound(playerSelection, getComputerChoice());
   }
   if (playerWins > computerWins) {
-    console.log("Congratualations! You won the best of 5 in Rock-Paper-Scissors!")
+    console.log("Congratulations! You won the best of 5 in Rock-Paper-Scissors!");
   }
   else if (playerWins < computerWins) {
-    console.log("Bummer! Better luck next time!")
+    console.log("Bummer! Better luck next time!");
   }
-  else console.log("Tie! Both you and the computer ended the game with the same points")
+  else console.log("Tie! Both you and the computer ended the game with the same points");
 }
 
 var playerWins = 0;
