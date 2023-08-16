1b1ddfb960d21d01dcc0eddb4a5f9e35e690ea32
Chii Yuen <chii.yuen@hotmail.com>
2023-08-16 23:15:55 +0800

    Add footer, improve index and script

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-function getComputerChoice() {
-  const choice = ["rock", "paper", "scissors"];
-  return computerChoice = choice[Math.floor(Math.random() * 3)];
+var playerChoice, computerChoice, round = playerWins = computerWins = 0, chosenChoices, outcome, playerScore, computerScore, endMessage;
+
+const main = document.querySelector('main');
+const intro = document.querySelector('.intro');
+const buttons = document.querySelectorAll('button');
+const results = document.querySelector('.results');
+
+function initialize() {
+  chosenChoices = document.createElement('h3');
+  outcome = document.createElement('p');
+  playerScore = document.createElement('p');
+  computerScore = document.createElement('p');
+  endMessage = document.createElement('h3');
+  results.append(chosenChoices, outcome, playerScore, computerScore, endMessage);
 }
 
-function roundCounter() {
-  round += 1;
-  roundsPlayed.textContent = `Round ${round}`;
+function incrementRound() {
+  round++;
+  intro.textContent = `Round ${round}`;
 }
 
-function roundOutcome() {
+function playRound() {
+  getComputerChoice();
   chosenChoices.textContent = `You chose ${playerChoice} and the computer chose ${computerChoice}...`;
   switch (true) {
     case (playerChoice == computerChoice):
@@ -17,79 +29,58 @@ function roundOutcome() {
     case (playerChoice == 'rock' && computerChoice == 'scissors'):
     case (playerChoice == 'paper' && computerChoice == 'rock'):
     case (playerChoice == 'scissors' && computerChoice == 'paper'):
-      firstWord = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1);
-      outcome.textContent = `${firstWord} beats ${computerChoice}! Nice one.`;
+      outcome.textContent = `${capitalize(playerChoice)} beats ${computerChoice}! Nice one!`;
       playerWins++;
       break;
     case (playerChoice == 'rock' && computerChoice == 'paper'):
     case (playerChoice == 'paper' && computerChoice == 'scissors'):
     case (playerChoice == 'scissors' && computerChoice == 'rock'):
-      firstWord = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
-      outcome.textContent = `${firstWord} beats ${playerChoice}! Unlucky...`;
+      outcome.textContent = `${capitalize(computerChoice)} beats ${playerChoice}! Unlucky...`;
       computerWins++;
       break;
   }
+  playerScore.textContent = `Player score = ${playerWins}`;
+  computerScore.textContent = `Computer score = ${computerWins}`;
+}
+
+function getComputerChoice() {
+  const choice = ["rock", "paper", "scissors"];
+  return computerChoice = choice[Math.floor(Math.random() * 3)];
+}
+
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 function hasGameEnded() {
   if (playerWins == 5 || computerWins == 5) {
     buttons.forEach(button => button.setAttribute('disabled', ""));
     const restart = document.createElement('button');
-    restart.textContent = 'Play Again?';
-    document.body.appendChild(restart);
-    restart.addEventListener('click', () => {
-      reset();
-      restart.remove();
-    });
+    restart.textContent = 'Play Again';
+    main.appendChild(restart);
     if (playerWins == 5) {
-      endMessage.textContent = "Congratulations! You won the best of 5 in Rock-Paper-Scissors!";
-    }
-    else {
+      endMessage.textContent = "Congratulations! You won the best of 5!";
+    } else {
       endMessage.textContent = "Bummer! Better luck next time!";
     }
+    restart.addEventListener('click', () => {
+      restart.remove();
+      round = playerWins = computerWins = 0;
+      while (results.firstChild) {
+        results.firstChild.remove();
+      }
+      buttons.forEach(button => button.removeAttribute('disabled'));
+      intro.textContent = "Select one of the moves below to start a new game...";
+    });
   }
 }
 
-function reset() {
-  round = 0, playerWins = 0, computerWins = 0;
-  while (results.firstChild) {
-    results.firstChild.remove();
+buttons.forEach(button => button.addEventListener('click', () => {
+  if (round == 0) {
+    initialize();
   }
-  buttons.forEach(button => button.removeAttribute('disabled'));
-  introMessage.style.display = 'block';
-}
-
-function initialize() {
-  roundsPlayed = document.createElement('h2');
-  chosenChoices = document.createElement('h2');
-  outcome = document.createElement('p');
-  playerScore = document.createElement('p');
-  computerScore = document.createElement('p');
-  endMessage = document.createElement('h3');
-  results.append(roundsPlayed, chosenChoices, outcome, playerScore, computerScore, endMessage);
-}
-
-function playGame() {
-  buttons.forEach(button => button.addEventListener('click', () => {
-    if (round == 0) {
-      initialize();
-      introMessage.style.display = 'none';
-    }
-    playerChoice = button.getAttribute('class');
-    getComputerChoice();
-    roundCounter();
-    roundOutcome();
-    hasGameEnded();
-    playerScore.textContent = `Player score = ${playerWins}`;
-    computerScore.textContent = `Computer score = ${computerWins}`;
-  }));
-}
-
-var playerChoice, computerChoice, firstWord, round = 0, playerWins = 0, computerWins = 0;
-var roundsPlayed, chosenChoices, outcome, playerScore, computerScore, endMessage;
-
-const introMessage = document.querySelector('.intro');
-const buttons = document.querySelectorAll('button');
-const results = document.querySelector('.results');
-
-playGame();
+  incrementRound();
+  playerChoice = button.value;
+  playRound();
+  hasGameEnded();
+}));
